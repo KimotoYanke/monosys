7c6d7eff87c5842e067445e86c9b95a2acf93605
adam <adam@example.com>
2017-06-05 23:16:09 +0900

    searching input

--- app/components/App.vue
+++ app/components/App.vue
@@ -3,9 +3,15 @@ div
 	nav.nav.has-shadow
 		.nav-left
 			a.nav-item(href='/', style='font-family: "Rubik";') Monosys
+		.nav-right
+			b-field.nav-item
+				b-input(
+					type='search'
+					icon='search')
+				p.control
+					button.button.is-primary 検索
 	thing-table(
-		:data='things'
-		)
+		:data='things')
 </template>
 <script>
 import ThingTable from './ThingTable.vue'
