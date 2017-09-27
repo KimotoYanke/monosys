d4df97e8de89de801c2e10ebaad7d1fa04c9f908
kimotoshin <kimotoshin>
2017-09-27 16:04:25 +0900

    Fix coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,82 +1,126 @@
-var webpack = require('webpack')
+var webpackConfig = require('./webpack.config.js')[0]
 
 module.exports = function (config) {
-	config.set({
+    config.set({
 
-		basePath: '',
+        basePath: '',
 
-		frameworks: ['mocha', 'chai'],
-		plugins: [
-			'karma-phantomjs-launcher',
-			'karma-mocha',
-			'karma-chai',
-			'karma-coverage',
-			'karma-coveralls',
-			'karma-mocha-reporter',
-			'karma-sourcemap-loader',
-			'karma-webpack'
-		],
+        frameworks: ['mocha', 'chai'],
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-mocha',
+            'karma-chai',
+            'karma-coverage',
+            'karma-coveralls',
+            'karma-mocha-reporter',
+            'karma-sourcemap-loader',
+            'karma-webpack'
+        ],
 
-		files: [
-			'app/test/index.spec.js'
-		],
+        files: [
+            'app/test/index.spec.js'
+        ],
 
-		exclude: [
-		],
+        exclude: [
+        ],
 
-		preprocessors: {
-			'app/test/*.spec.js': ['webpack', 'sourcemap']
-		},
+        preprocessors: {
+            'app/test/*.spec.js': ['webpack', 'sourcemap']
+        },
 
-		reporters: ['mocha', 'coverage', 'coveralls'],
+        reporters: ['mocha', 'coverage', 'coveralls'],
 
-		coverageReporter: {
-			reporters: [
-				{ type: 'text' },
-				{ type: 'lcov' },
-				{ type: 'html' }
-			]
-		},
+        coverageReporter: {
+            reporters: [
+                { type: 'text' },
+                { type: 'lcov' },
+                { type: 'html' }
+            ]
+        },
 
-		port: 9876,
+        port: 9876,
 
-		colors: true,
+        colors: true,
 
-		logLevel: config.LOG_INFO,
+        logLevel: config.LOG_INFO,
 
-		autoWatch: false,
+        autoWatch: false,
 
-		browsers: ['PhantomJS'],
+        browsers: ['PhantomJS'],
 
-		singleRun: false,
-		concurrency: Infinity,
-		mime: {
-			'text/x-typescript': ['ts','tsx'],
-			'text/javascript': ['js'],
-		},
+        singleRun: false,
+        concurrency: Infinity,
+        mime: {
+            'text/x-typescript': ['ts', 'tsx'],
+            'text/javascript': ['js']
+        },
 
-		webpack: {
-			module: {
-				rules: [
-					{
-						test: /\.vue$/,
-						loaders: ['istanbul-instrumenter-loader', 'vue-loader']
-					},
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						loaders: ['istanbul-instrumenter-loader', 'babel-loader']
-					}
-				]
+        webpack: {
+            entry: './app/src/main.js',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['es2015']
+                            }
+                        }
+                    },
+                    {
+                        test: /\.vue$/,
+                        use: {
+                            loader: 'vue-loader',
+                            options: {
+                                scss: 'vue-style-loader!css-loader!sass-loader',
+                                sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                            }
+                        }
+                    },
+                    {
+                        test: /\.pug$/,
+                        use: {
+                            loader: 'pug-loader'
+                        }
+                    },
+                    {
+                        test: /\.json$/,
+                        use: {
+                            loader: 'json-loader'
+                        }
+                    },
+                    {
+                        test: /\.css$/,
+                        use: [{
+                            loader: 'style-loader'
+                        }, {
+                            loader: 'css-loader'
+                        }]
+                    },
+                    {
+                        test: /\.sass$/,
+                        use: [{
+                            loader: 'style-loader'
+                        }, {
+                            loader: 'css-loader'
+                        }, {
+                            loader: 'sass-loader',
+                            options: {
+                            }
+                        }]
+                    }
+                ]
 
-			},
-			resolve: {
-				alias: {
-					vue: 'vue/dist/vue.js'
-				},
-				extensions: ['.js', '.vue', '.json']
-			},
-			devtool: 'inline-source-map'
-		}
-	})
+            },
+            resolve: {
+                alias: {
+                    vue: 'vue/dist/vue.js'
+                },
+                extensions: ['.js', '.vue', '.json']
+            },
+            devtool: 'inline-source-map'
+        }
+    })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,105 +1,105 @@
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = [{
-	entry: './app/src/main.js',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015']
-					}
-				}
-			},
-			{
-				test: /\.vue$/,
-				use: {
-					loader: 'vue-loader',
-					options: {
-						scss: 'vue-style-loader!css-loader!sass-loader',
-						sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-					}
-				}
-			},
-			{
-				test: /\.pug$/,
-				use: {
-					loader: 'pug-loader'
-				}
-			},
-			{
-				test: /\.json$/,
-				use: {
-					loader: 'json-loader'
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [{
-					loader: 'style-loader'
-				}, {
-					loader: 'css-loader'
-				}]
-			},
-			{
-				test: /\.sass$/,
-				use: [{
-					loader: "style-loader"
-				}, {
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader",
-					options: {
-					}
-				}]
-			}
-		]
+    entry: './app/src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader',
+                    options: {
+                        scss: 'vue-style-loader!css-loader!sass-loader',
+                        sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                }
+            },
+            {
+                test: /\.pug$/,
+                use: {
+                    loader: 'pug-loader'
+                }
+            },
+            {
+                test: /\.json$/,
+                use: {
+                    loader: 'json-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }]
+            },
+            {
+                test: /\.sass$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                    }
+                }]
+            }
+        ]
 
-	},
-	resolve: {
-		alias: {
-			vue: 'vue/dist/vue.js'
-		},
-		extensions: ['.js', '.vue', '.json']
-	},
-	devtool: 'inline-source-map'
-}
-	,{
-		entry: './app/src/stylesheets/index',
-		output: {
-			path: path.resolve(__dirname, './dist'),
-			filename: 'bundle.css'
-		},
-		module: {
-			loaders: [
-				{
-					test: /\.css$/,
-					loader: ExtractTextPlugin.extract({fallback:'style-loader', use:'css-loader'})
-				},
-				{
-					test: /\.scss$/,
-					loader: ExtractTextPlugin.extract({fallback:'style-loader', use:'css-loader!sass-loader'})
-				},
-				{
-					test: /\.sass$/,
-					loader: ExtractTextPlugin.extract({fallback:'style-loader', use:'css-loader!sass-loader'})
-				}
-			]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        },
+        extensions: ['.js', '.vue', '.json']
+    },
+    devtool: 'inline-source-map'
+},
+{
+    entry: './app/src/stylesheets/index',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.css'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' })
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' })
+            }
+        ]
 
-		},
-		resolve: {
-			extensions: ['.css','.sass','.scss','.js']
-		},
-		plugins:[
-			new ExtractTextPlugin("[name].css")
-		],
-		devtool: 'inline-source-map'
-	}]
+    },
+    resolve: {
+        extensions: ['.css', '.sass', '.scss', '.js']
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ],
+    devtool: 'inline-source-map'
+}]
