e9e2eb4e8b4202238d5246f75f43c3e45b200fda
kimotoyanke <kimotoyanke@gmail.com>
2017-10-12 17:49:10 +0900

    Fix to return at Vuex actions

--- app/src/store/thing/actions.js
+++ app/src/store/thing/actions.js
@@ -2,7 +2,7 @@ import types from './mutations-type.json'
 import axios from 'axios'
 
 export function fetch ({ commit }, query) {
-    axios.get('/api/v1/thing', {
+    return axios.get('/api/v1/thing', {
         params: query
     }).then(responce => {
         commit(types.fetch, {
--- app/src/store/user/actions.js
+++ app/src/store/user/actions.js
@@ -2,21 +2,21 @@ import types from './mutations-type.json'
 import axios from 'axios'
 
 export function login ({ dispatch }, query) {
-    axios.post('/user/login', query)
+    return axios.post('/user/login', query)
         .then(responce => {
             dispatch('checkLoggedIn')
         })
 }
 
 export function register ({ dispatch }, query) {
-    axios.post('/user/register', query)
+    return axios.post('/user/register', query)
         .then(responce => {
             dispatch('checkLoggedIn')
         })
 }
 
 export function checkLoggedIn ({ commit }) {
-    axios.get('/user/loggedin').then(responce => {
+    return axios.get('/user/loggedin').then(responce => {
         commit(types.setUsername, {
             username: responce.data
         })
@@ -24,7 +24,7 @@ export function checkLoggedIn ({ commit }) {
 }
 
 export function logout ({ dispatch }) {
-    axios.get('/user/logout').then(responce => {
+    return axios.get('/user/logout').then(responce => {
         dispatch('checkLoggedIn')
     })
 }
